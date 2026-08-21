01252c19b179c1d50c974f5637d1d9cbc043c281
JoshuaHCole <inventor@aios.is>
2026-08-21 16:45:18 -0700

    Fix fine-line tattoo FAQ wording

--- tools/seo_qa.py
+++ tools/seo_qa.py
@@ -21,7 +21,7 @@ FORBIDDEN = {
     "deprecated phone 725-260-6376": r"725[-\s.]260[-\s.]6376",
     "deprecated phone 702-960-9607": r"702[-\s.]960[-\s.]9607",
     "legacy email": r"Thewhiteknight702@gmail\.com",
-    "tattoo/piercing contamination": r"where\s+do\s+you\s+pierce\b|pierce\s+fine\s+line\s+tattoo|where\s+do\s+you\s+pierce\s+fine\s+line",
+    "tattoo/piercing contamination": r"where\s+do\s+you\s+(pierce|tattoo)\b|where\s+do\s+you\s+pierce\s+[^?<]{0,80}\btattoo\b|pierce\s+(fine[-\s]?line|realism|cover[-\s]?up)\s+tattoo",
 }
 
 def route_for(path: Path) -> str:
